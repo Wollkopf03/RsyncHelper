9a5bfc6f60539cc1e27c0f9fc868bb4164b94784
Wollkopf03 <lukaswollmann03@gmx.de>
2022-07-20 16:02:24 +0200

    Added utils.py

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import subprocess
+from inspect import currentframe, getframeinfo
+
+
+def cmd(bashCommand):
+    process = subprocess.run(bashCommand.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.stdout, process.stderr
+    print("------------Begin-Command------------\n" + bashCommand)
+    print("Output: " + output.decode("utf-8") + "\nError: " + error.decode("utf-8"))
+    print("-------------End-Command-------------")
+    return (output, error)
+
+def debug(dbgMsg):
+    frameinfo = getframeinfo(currentframe())
+    print(frameinfo.filename + "(" + frameinfo.lineno + ")" + ": " + dbgMsg)
